ef60067648f42a95e7cfd6ece95fb2baadc882bf
Nicolas Casas <nicolas.casas@usantoto.edu.co>
2021-03-26 10:48:38 -0500

    Upload Files

--- JAVA/P1T36_SYSTEM_Out_print/src/usta/fis/Main.java
+++ JAVA/P1T36_SYSTEM_Out_print/src/usta/fis/Main.java
@@ -5,17 +5,35 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("_______SSSS");
-        System.out.println("-----SSSSSSSSS");
-        System.out.println("-----SSSSSSS-S");
-        System.out.println("----SSSSSSSSSS");
+        System.out.println("_____SSSSSSSSS");
+        System.out.println("_____SSSSSSS_S");
+        System.out.println("____SSSSSSSSSS");
         System.out.println("_____SSSSSSSSSS");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
-        System.out.println("");
+        System.out.println("____SSSSSS_SSSSS");
+        System.out.println("___SSSSSSS_____SSS");
+        System.out.println("___SSSSSSSS_____S");
+        System.out.println("___SSSSSSSSSS");
+        System.out.println("____SSSSSSSSSS");
+        System.out.println("____SSSSSSSSSSS");
+        System.out.println("_____SSSSSSSSSSS");
+        System.out.println("_SSSS__SSSSSSSSS");
+        System.out.println("__SSSSSSSSSSSSSS");
+        System.out.println("_SSSSSSSSSSSSSS");
+        System.out.println("__SSSSSSSSSSSS");
+        System.out.println("SSSSSSSSSSSS");
+        System.out.println("__S__SSSSSS");
+        System.out.println("____SSSSSS");
+        System.out.println("____SSSSS");
+        System.out.println("___SSSSSS_____S");
+        System.out.println("___SSSSSS___SS_SS");
+        System.out.println("____SSSSS___S__SS");
+        System.out.println("____SSSSS______SS");
+        System.out.println("_____SSSSS____SSS");
+        System.out.println("_______SSSSSSSSS");
+        System.out.println("__________SSSS");
+
+
+
+
     }
 }
---
+++ JAVA/P1T37_Methods/src/usta/fis/Main.java
@@ -0,0 +1,21 @@
+package usta.fis;
+//AUTHOR     :  Nicolas Casas Bello
+//DESCRIPTION:  This software gonna calculate the time of a bicycle
+//DATE       :  25/03/2021
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        p_show_info_program();
+    }
+    public static void p_show_info_program() {
+        //DESCRIPTION:This method shows the software info
+        //DATE:2021-March-23
+        System.out.println("--------------------------------------");
+        System.out.println("         SOFTMETHOD VERSION 1.0       ");
+        System.out.println("          Nicolás Casas Bello         ");
+        System.out.println("           DESCR: Use Methods         ");
+        System.out.println("--------------------------------------");
+    }
+}
